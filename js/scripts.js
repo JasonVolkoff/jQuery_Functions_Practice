4379e58a68794292be54556d9390571f57a9684d
JasonVolkoff <jasonvolkoff@mail.fresnostate.edu>
2021-03-10 21:51:20 -0800

    Practiced jQuery functions: slideToggle, slideUp, slideDown.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,33 @@ $(document).ready(function () {
     $("#classRed").click(function () {
         $("#redPara").css("color", "red");
     })
+
+    $("#classAppend").click(function () {
+        $("#appendPara").append(".(^.^)./");
+    })
+
+    $("#classToggle").click(function () {
+        $("#togglePara").slideToggle();
+    })
+
+    // $("#classHide").click(function () {
+    //     $("#showPara").hide("fast", function () {
+    //         $(this).prev().hide("fast", arguments.callee);
+    //     });
+    $("#classHide").click(function () {
+        $("#showPara").hide("fast")
+    });
+    $("#classShow").click(function () {
+        $("#showPara").show("fast");
+    })
+
+    $("#classSlideUp").click(function () {
+        if ($("#slideUpPara").first().is(":hidden")) {
+            $("#slideUpPara").slideDown("slow");
+        }
+        else {
+            $("#slideUpPara").slideUp();
+        }
+    })
 })
+
